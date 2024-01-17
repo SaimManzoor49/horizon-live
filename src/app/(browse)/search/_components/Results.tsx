bc000061faa5f6a0665081965cf0311d7d4fabb0
unknown <saummanzoor49@gmail.com>
2024-01-17 23:42:45 +0500

    result-card

--- src/app/(browse)/search/_components/Results.tsx
+++ src/app/(browse)/search/_components/Results.tsx
@@ -1,22 +1,40 @@
+import { getSearch } from '@/lib/search-service'
 import React from 'react'
+import ResultCard from './ResultCard'
 
-interface ResultsProps{
-    term?:string
+interface ResultsProps {
+    term?: string
 }
 
-const Results = ({term}:ResultsProps) => {
-  return (
-    <div >
-        <h2 className='text-lg font-semibold mb-4'>
-        Results for term &quot;{term}&quot;
-        </h2>
-    </div>
-  )
+const Results = async ({ term }: ResultsProps) => {
+
+    const data = await getSearch(term)
+
+    return (
+        <div >
+            <h2 className='text-lg font-semibold mb-4'>
+                Results for term &quot;{term}&quot;
+            </h2>
+            {data.length === 0 && (
+                <p className="text-muted-foreground text-sm">
+                    No results found. Try searching for something else
+                </p>
+            )}
+            <div className="flex flex-col gap-y-4">
+                {data.map((result) => (
+                    <ResultCard
+                        key={result.id}
+                        data={result}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Results
-export const ResultsSkeleton = ()=>{
-    return(
+export const ResultsSkeleton = () => {
+    return (
         <div className=""></div>
     )
 }
